e35d67c91766a3e3d2fa42579f0b919829e1f467
Rahmat Tobi Hidayat <49316783+rahmattobi@users.noreply.github.com>
2019-04-06 11:02:47 +0700

    Update Github8.java

--- Github8.java
+++ Github8.java
@@ -19,7 +19,7 @@ public class Github8 {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner fibonanci = new Scanner(System.in);
-  
+        
         int n = fibonanci.nextInt();
         int fib[] = new int[n];
          
